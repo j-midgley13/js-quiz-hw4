dd665e063cba3ee4cdd4ddfeb5cbfb18e05793f8
Jameson Midgley <jameson.midgley@gmail.com>
2020-06-20 01:23:59 -0600

    Changed and added questions. Got timer to count down.

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ var timer = document.getElementById("timer");
 var questionDiv = document.getElementById("question-div");
 var questionsList = document.getElementById("questions");
 var question = document.getElementById("question-display");
+var seconds = 60;
+var interval;
+var i = 0;
 
 var arrayQuestions = [
     {
-        q: "What HTML element does JavaScript use?",
+        q: "What HTML element does JavaScript live in?",
         options: ["<body>", "<div>", "<script>"],
         a: "<script>"
     },
@@ -20,9 +23,15 @@ var arrayQuestions = [
     },
 
     {
-        q: "How do you declare a string?",
-        options: ["Quotation Marks", "Square Brackets", "Parentheses"],
-        a: "Quotation Marks"
+        q: "What symbol is used for adding comments in Javascript?",
+        options: ["<-- add comment here -->", "// add comment here", "*/ add comment here"],
+        a: "// add comment here"
+    },
+
+    {
+        q: "How do you create a DIV element in JavaScript?",
+        options: ["document.createElement('div');", "div = createElement.body();", "You cannot create a div."],
+        a: "document.createElement('div');"
     },
 
     {
@@ -31,3 +40,22 @@ var arrayQuestions = [
         a: "Brendan Eich"
     },
 ]
+
+function startTimer() {
+    interval = setInterval( function() {
+        seconds --;
+        timer.innerHTML = ("Timer: " + seconds);
+        // console.log(seconds);
+        if (seconds < 0){
+            stopTimer();
+            alert("Gotta be quicker than that!");
+        }
+    }, 1000);
+};
+
+function stopTimer() {
+    clearInterval(interval);
+};
+
+startBtn.addEventListener("click", startTimer);
+// startBtn.addEventListener("click", startQuestions);
